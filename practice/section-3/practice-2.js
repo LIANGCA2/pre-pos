918baeefc7c9cbcd3b7ee4cfcc32c83b1ceedb32
CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-03 20:59:34 +0800

    Carol for section3 practice2

--- practice/section-3/practice-2.js
+++ practice/section-3/practice-2.js
@@ -1,5 +1,28 @@
 'use strict';
 
 function createUpdatedCollection(collectionA, objectB) {
-  return '实现练习要求，并改写该行代码。';
+  var target =new Array();
+  var collectionB = objectB.value
+  for(var i = 0;i<collectionA.length;i++ ){
+    var array = collectionA[i];
+    var _key = array.key;
+    if(containKey(collectionB,_key)){
+      if(array.count>=3)
+      array.count = array.count-parseInt(array.count/3);
+    }
+    collectionA[i] = array;
+  }
+  return collectionA;
+}
+//判断Map中是否含有指定key的元素
+function containKey(collectionB,_key) {
+  var bln = false;
+
+  for(var i = 0;i<collectionB.length;i++){
+    if (collectionB[i] == _key){
+      bln = true;break;
+    }
+
+  }
+  return bln;
 }
