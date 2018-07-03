70ff040765fa56b3e60cf29581bbd4dec611e2c7
CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-03 21:15:57 +0800

    Carol for section1

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,19 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+  var target = new Array()
+  for (var i = 0; i < collectionA.length; i++) {
+    for (var j = 0; j < collectionB.length; j++) {
+      var childrenArray = collectionB[j];
+      for (var k = 0; k < childrenArray.length; k++) {
+        if (collectionA[i] == childrenArray[k]) {
+          target.push(collectionA[i]);
+          break;
+        }
+      }
+
+
+    }
+  }
+  return target;
 }
