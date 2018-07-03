f70637253b07f99991ccf6aaa4c790a988797075
CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) <CAROL T T LIANG (ITA-ISDC-ISD-OOCLL/ZHA) carol.t.t.liang@oocl.com>
2018-07-03 19:59:21 +0800

    Carol for section2 practice1

--- practice/section-2/practice-1.js
+++ practice/section-2/practice-1.js
@@ -1,5 +1,31 @@
 'use strict';
 
 function countSameElements(collection) {
-  return '实现练习要求，并改写该行代码。';
+  var target =new Array();
+  var map = {};
+  for(var i = 0;i<collection.length;i++){
+    var key = collection[i]
+    if(containKey(map,key)){
+      map[key] =  map[key]+1;
+    }else{
+      map[key]=1;
+    }
+  }
+  for(var key in map) {
+   var array = {key: key, count: map[key]}
+   target.push(array);
+  }
+  return target;
+}
+//判断Map中是否含有指定key的元素
+function containKey(map,_key) {
+  var bln = false;
+
+    for(var key in map){
+      if (key == _key){
+        bln = true;break;
+      }
+
+  }
+  return bln;
 }
